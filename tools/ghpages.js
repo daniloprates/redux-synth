53c8077f9beb4b25e8d8f51e9fade4711e63a71d
Danilo Prates <cin_dprates@gmail.com>
2016-10-25 19:37:36 -0200

    build

--- tools/ghpages.js
+++ tools/ghpages.js
@@ -1,7 +1,7 @@
-var ghpages = require('gh-pages');
-var path = require('path');
+/* eslint-disable no-console */
 
-
+let ghpages = require('gh-pages');
+let path = require('path');
 
 ghpages.publish(path.join(__dirname, '../', 'dist'), function(err) {
   console.log(err || 'Success!');
